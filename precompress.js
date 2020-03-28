3746192ec78ed682742134586b2c736c5b0f97ed
silverwind <me@silverwind.io>
2020-03-28 23:34:32 +0100

    update dependencies

--- precompress.js
+++ precompress.js
@@ -130,7 +130,7 @@ async function main() {
   for (const file of args._) {
     const stats = await stat(file);
     if (stats.isDirectory()) {
-      for await (const entry of rrdir.stream(file, rrdirOpts)) {
+      for await (const entry of rrdir(file, rrdirOpts)) {
         if (!entry.directory) files.push(entry.path);
       }
     } else {
