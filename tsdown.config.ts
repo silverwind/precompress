f1dbd5521d745a681cecc66435359706f90bb8b4
silverwind <me@silverwind.io>
2025-09-29 07:53:03 +0200

    switch to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,8 @@
+import {nodeCli} from "tsdown-config-silverwind";
+import {defineConfig} from "tsdown";
+
+export default defineConfig(nodeCli({
+  url: import.meta.url,
+  minify: true,
+  sourcemap: false,
+}));
